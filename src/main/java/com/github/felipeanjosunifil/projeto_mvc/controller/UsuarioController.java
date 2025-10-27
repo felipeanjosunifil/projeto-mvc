093a60ebcd0a4af85c62efa0567387a107539838
lipenathan <felipenatthan@gmail.com>
2025-10-27 20:15:45 -0300

    código 27-10

--- src/main/java/com/github/felipeanjosunifil/projeto_mvc/controller/UsuarioController.java
+++ src/main/java/com/github/felipeanjosunifil/projeto_mvc/controller/UsuarioController.java
@@ -1,16 +1,77 @@
 package com.github.felipeanjosunifil.projeto_mvc.controller;
 
 import com.github.felipeanjosunifil.projeto_mvc.model.Usuario;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UsuarioController {
 
+    private int ID = 0;
+    private List<Usuario> usuarios = new ArrayList<>();
+
     @PostMapping("/usuarios/novo")
-    public boolean criarUsuario(@RequestBody Usuario usuario) {
-        System.out.println(usuario);
-        return true;
+    public ResponseEntity<Boolean> criarUsuario(@RequestBody Usuario usuario) {
+        usuario.setId(++ID);
+        usuarios.add(usuario);
+        return ResponseEntity.status(201).body(true);
     }
+
+    @GetMapping("/usuarios")
+    public ResponseEntity<List<Usuario>> getUsuarios() {
+        return ResponseEntity.ok(usuarios);
+    }
+
+    @DeleteMapping("/usuarios/{id}")
+    public ResponseEntity<Boolean> deletarUsuarioPorId(@PathVariable("id") int id) {
+        Usuario usuarioDelecao = null;
+
+        for (Usuario usuario : usuarios) {
+            if (usuario.getId() == id) {
+                usuarioDelecao = usuario;
+            }
+        }
+
+        if (usuarioDelecao != null) {
+            usuarios.remove(usuarioDelecao);
+            return ResponseEntity.ok(true);
+        } else {
+            return ResponseEntity.status(404).body(false);
+        }
+    }
+
+    @PutMapping("/usuarios")
+    public ResponseEntity<Boolean> atualizarUsuarioPorId(@RequestBody Usuario usuarioAtual) {
+//        boolean atualizado = false;
+        Usuario usuarioAtualiacao = null;
+        int index = 0;
+
+        for (Usuario usuario : usuarios) {
+            if (usuarioAtualiacao == null) {
+                index++;
+            }
+            if (usuario.getId() == usuarioAtual.getId()) {
+                usuarioAtualiacao = usuario;
+            }
+        }
+
+        if (usuarioAtualiacao != null) {
+            usuarios.set(index-1, usuarioAtual);
+            return ResponseEntity.ok(true);
+        } else {
+            return ResponseEntity.badRequest().body(false);
+        }
+    }
+
+    private Usuario atualizarUsuario(Usuario usuarioAntigo, Usuario usuarioAtual) {
+        usuarioAntigo.setNome(usuarioAtual.getNome());
+        usuarioAntigo.setEmail(usuarioAtual.getEmail());
+        usuarioAntigo.setSenha(usuarioAtual.getSenha());
+
+        return usuarioAntigo;
+    }
+
 }
