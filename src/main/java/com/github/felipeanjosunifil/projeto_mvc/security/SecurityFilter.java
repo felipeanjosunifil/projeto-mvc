5a9ead6d6515a742825ac28777cba43d5017bf0c
lipenathan <felipenatthan@gmail.com>
2026-02-18 20:06:41 -0300

    spring security basic.

---
+++ src/main/java/com/github/felipeanjosunifil/projeto_mvc/security/SecurityFilter.java
@@ -0,0 +1,48 @@
+package com.github.felipeanjosunifil.projeto_mvc.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityFilter {
+
+    @Bean
+    public SecurityFilterChain securityFIlterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(authorization -> {
+                    authorization.requestMatchers(HttpMethod.POST,"/usuarios/**").hasRole("ADMIN");
+                    authorization.requestMatchers("/login").permitAll();
+                    authorization.anyRequest().authenticated();
+                })
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user1 = User.builder().username("user").password(passwordEncoder.encode("1234")).roles("USER").build();
+        UserDetails user2 = User.builder().username("admin").password(passwordEncoder.encode("qwert")).roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+}
