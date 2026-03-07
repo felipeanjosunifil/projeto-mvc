07bd3f04ecda5419a0730d8ee1a62b24f2495233
lipen <felipe.anjos@unifil.br>
2026-03-07 10:51:46 -0300

    stateless

--- src/main/java/com/github/felipeanjosunifil/projeto_mvc/security/SecurityFilter.java
+++ src/main/java/com/github/felipeanjosunifil/projeto_mvc/security/SecurityFilter.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,6 +25,7 @@ public class SecurityFilter {
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorization -> {
                     authorization.requestMatchers(HttpMethod.POST,"/usuarios/novo").permitAll();
                     authorization.requestMatchers(HttpMethod.POST,"/papel/novo").permitAll();
